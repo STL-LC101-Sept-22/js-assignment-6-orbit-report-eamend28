6be35236b06602c92f28d45b90acfd08d863ddef
eamend28 <117699959+eamend28@users.noreply.github.com>
2022-12-08 22:35:51 -0600

    Update satellite.ts

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,9 +15,10 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-		return true;
+		return this.type.toLowerCase() === 'space debris';;
    }
 
 }
 
 // TODO 3a: fix isSpaceDebris check
+
